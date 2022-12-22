89465f0b5e0542fff5db7f61a2bd5a9da0e2ad83
Gapeev <dizzikz@gmail.com.com>
2022-12-22 15:34:42 +0300

    сделал автоматическую дозагрузку коктейлей

--- CocktailsApp_react-redux/src/features/CocktailCardList/CocktailCardList.js
+++ CocktailsApp_react-redux/src/features/CocktailCardList/CocktailCardList.js
@@ -1,5 +1,5 @@
 import styles from './CocktailCardList.module.scss';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchCockailsByIngredient } from '../../app/coctailsSlice';
 import CocktailCard from '../CocktailCard/CocktailCard';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,11 +10,32 @@ const CocktailCardList = () => {
     (state) => state.cocktailsSlice
   );
   const { currentIngredient } = useSelector((state) => state.ingredientsSlice);
+  const initialQtyLoadCocktails = 24;
+  const [qtyLoadCocktails, setQtyLoadCocktails] = useState(
+    initialQtyLoadCocktails
+  );
 
   useEffect(() => {
+    setQtyLoadCocktails(initialQtyLoadCocktails);
     document.querySelector('.activeBlock').scrollTop = 0;
     dispatch(fetchCockailsByIngredient(currentIngredient));
   }, [currentIngredient]);
+
+  useEffect(() => {
+    const cocktailsListDiv = document.querySelector('.activeBlock');
+    const cocktailsListUl = document.querySelector('.cocktailsListUl');
+    const autoLoadCocktails = () => {
+      if (
+        cocktailsListUl.getBoundingClientRect().bottom <
+        cocktailsListDiv.clientHeight + 350
+      ) {
+        setQtyLoadCocktails((qtyLoadCocktails) => qtyLoadCocktails + 6);
+      }
+    };
+    cocktailsListDiv.addEventListener('scroll', autoLoadCocktails);
+    return () =>
+      cocktailsListDiv.removeEventListener('scroll', autoLoadCocktails);
+  }, [qtyLoadCocktails]);
 
   return (
     <div className={styles.body}>
@@ -22,19 +43,24 @@ const CocktailCardList = () => {
         <span>{currentIngredient}</span>
       </div>
       <div className={`${styles.contentBlock} activeBlock`}>
-        <ul className={styles.list}>
+        <ul className={`${styles.list} cocktailsListUl`}>
           {cocktailsByIngredient.errorStatus && (
             <h2>{cocktailsByIngredient.errorStatus}</h2>
           )}
           {cocktailsByIngredient.cocktails?.map(
-            ({ strDrink, strDrinkThumb, idDrink }) => (
-              <CocktailCard
-                key={strDrink}
-                idDrink={idDrink}
-                strDrink={strDrink}
-                strDrinkThumb={strDrinkThumb}
-              />
-            )
+            ({ strDrink, strDrinkThumb, idDrink }, index) => {
+              if (index < qtyLoadCocktails) {
+                // console.log(qtyLoadCocktails, index);
+                return (
+                  <CocktailCard
+                    key={strDrink}
+                    idDrink={idDrink}
+                    strDrink={strDrink}
+                    strDrinkThumb={strDrinkThumb}
+                  />
+                );
+              }
+            }
           )}
         </ul>
       </div>
